7803d41d2f3616d931011f5fe47fea5a3a00d78f
Albert Gomà <58812649+AlbertGoma@users.noreply.github.com>
2020-02-29 15:14:27 +0000

    Create irpf.sql

---
+++ irpf.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `irpf`;
+
+CREATE TABLE `irpf` (
+  `idIrpf` int(11) NOT NULL AUTO_INCREMENT,
+  `tipus` varchar(45) NOT NULL,
+  PRIMARY KEY (`idIrpf`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
