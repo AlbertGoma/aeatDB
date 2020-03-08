db444247ed33b59a70e29f0a5b7613aa25f985bb
Albert Gomà <58812649+AlbertGoma@users.noreply.github.com>
2020-03-08 16:45:05 +0000

    DRY

--- get349.sql
+++ get349.sql
@@ -2,25 +2,9 @@ DELIMITER ;;
 CREATE PROCEDURE `get349`(IN exercici YEAR, IN trimestre TINYINT UNSIGNED)
 BEGIN
   DECLARE inici,
-          fi    date;
+          fi    DATE;
   
-  CASE trimestre
-    WHEN 1 THEN
-      SET inici :=  date(concat(exercici, '-01-01')),
-          fi    :=  date(concat(exercici, '-03-31'));
-    WHEN 2 THEN
-      SET inici :=  date(concat(exercici, '-04-01')),
-          fi    :=  date(concat(exercici, '-06-30'));
-    WHEN 3 THEN
-      SET inici :=  date(concat(exercici, '-07-01')),
-          fi    :=  date(concat(exercici, '-09-30'));
-    WHEN 4 THEN
-      SET inici :=  date(concat(exercici, '-10-01')),
-          fi    :=  date(concat(exercici, '-12-31'));
-    ELSE
-      SET inici :=  date(concat(exercici, '-01-01')),
-          fi    :=  date(concat(exercici, '-12-31'));
-  END CASE;
+  CALL periode(exercici, trimestre, inici, fi);
 
   SELECT pv.iso 'Código País', v.nif 'NIF comunitario', v.raoSocial 'Razón social', SUM(f.baseImp) 'Base imponible'
   FROM factura f, proveidor v, pais pv, proveidor c, config cg
